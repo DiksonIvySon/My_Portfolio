72c40fcbde594d16dcdb25df2210409f610f6a8d
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-09-12 21:34:19 +0300

    Made mobile toggle icon to change when toggling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let primaryNavigation = document.querySelector('.primary-navigation');
 let mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+let mobileNavToggle_span = document.querySelector('.mobile-nav-toggle-span');
 
 mobileNavToggle.addEventListener('click', () => {
     let visibility = primaryNavigation.getAttribute('data-visible');
@@ -9,9 +10,13 @@ mobileNavToggle.addEventListener('click', () => {
     if (visibility === "false") {
         primaryNavigation.setAttribute('data-visible', true);
         mobileNavToggle.setAttribute('aria-expanded', true);
+        mobileNavToggle_span.textContent = "";
+        mobileNavToggle_span.innerHTML = `<i class="fa-solid fa-xmark"></i>`
     }
     else if (visibility === "true") {
         primaryNavigation.setAttribute('data-visible', false);
         mobileNavToggle.setAttribute('aria-expanded', false);
+        mobileNavToggle_span.textContent = "";
+        mobileNavToggle_span.innerHTML = `<i class="fa-solid fa-bars"></i>`
     }
 });
