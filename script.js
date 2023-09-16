4b723c809cbc810aa069bd41d9e83e564a77f5f1
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-09-16 15:39:09 +0300

    MAde the testimonials slideshow functional using JavaScript

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ window.addEventListener('scroll', function() {
     progress = (scr/fullHeight)*100;
     document.querySelector('.scrollbar').style.width = `${progress}vw`
 })
+
+// Code for the testimonials slide ................................................................
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
