62dd3d19cc644fead1bd44d14e938505aafcb148
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2024-06-25 17:44:32 +0300

    Updated the contact form with validation and animation.

--- contact.js
+++ contact.js
@@ -1,8 +1,81 @@
 //function to toggle to contact pop up
-let popUp_container = document.getElementById("container");
+/*let popUp_container = document.getElementById("container");
 let popUp_overlay_container = document.getElementById("overlayCon");
 let popUp_overlayBTN = document.querySelector(".overlayBtn");
 
 popUp_overlayBTN.addEventListener('click', ()=> {
   popUp_container.classList.toggle('right-panel-active');
+})*/
+
+const form = document.getElementById('form');
+const messengerName = document.getElementById('name');
+const email = document.getElementById('email');
+const subjectLine = document.getElementById('subjectLine');
+const message = document.getElementById('message');
+
+const name_error = document.getElementById('name_error');
+const email_error = document.getElementById('email_error');
+const subjectLine_error = document.getElementById('subjectLine_error');
+const message_error = document.getElementById('message_error');
+
+const successfulFormPopup = document.getElementById('successful-form-popup')
+
+
+form.addEventListener('submit',(e) => {
+  let emailStatus = true;
+
+  if (emailStatus) {
+    if (messengerName.value === '' || messengerName.value === null) {
+      e.preventDefault();
+      name_error.innerHTML = "Name is required";
+      emailStatus = false;
+    }else {
+      name_error.innerHTML = "";
+    }
+  
+    const emailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (!email.value.match(emailRegex)) {
+      e.preventDefault();
+      email_error.innerHTML = "A valid email is required";
+      emailStatus = false;
+    }else {
+      email_error.innerHTML = "";
+    }
+  
+    if (subjectLine.value === '' || subjectLine.value === null) {
+      e.preventDefault();
+      subjectLine_error.innerHTML = "A descriptive subject line is required";
+      emailStatus = false;
+    }else {
+      subjectLine_error.innerHTML = "";
+    }
+  
+    if (message.value === '' || message.value === null) {
+      e.preventDefault();
+      message_error.innerHTML = "A message is required";
+      emailStatus = false;
+    }else {
+      message_error.innerHTML = "";
+    }
+  }
+
+  //Display the success message
+  if (emailStatus) {
+    form.style.display = "none";
+    successfulFormPopup.style.display = "block";
+    e.preventDefault();
+  }
 })
+
+const sendAnotherEmail = document.getElementById('sendAnotherEmail');
+sendAnotherEmail.addEventListener('click', () => {
+  //clear the form
+  messengerName.value = "";
+  email.value = "";
+  subjectLine.value = "";
+  message.value = "";
+
+  //display the clear form
+  form.style.display = "block";
+  successfulFormPopup.style.display = "none";
+});
